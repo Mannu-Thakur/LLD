d22ad32515ea9da498089ddfff9a04a40393e4f0
Mannu Kumar Thakur <thakurmannu656@gmail.com>
2026-03-29 10:03:41 +0530

    Update enums.h with chess enumerations

--- chess_system/headers/enums.h
+++ chess_system/headers/enums.h
@@ -1,3 +1,17 @@
-# Enums
+#ifndef ENUMS_H
+#define ENUMS_H
 
-This header file contains enumerations used throughout the chess project.
+// Enums for better type safety
+enum Color {
+    WHITE, BLACK
+};
+
+enum PieceType {
+    KING, QUEEN, ROOK, BISHOP, KNIGHT, PAWN
+};
+
+enum GameStatus {
+    WAITING, IN_PROGRESS, COMPLETED, ABORTED
+};
+
+#endif // ENUMS_H
